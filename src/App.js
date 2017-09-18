3408e4fa866bc20f7746d55dff8fec20ca66a29b
Burcu Noyan <burcunoyan@gmail.com>
2017-09-17 21:46:34 -0400

    create App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App  = () => {
+  return (
+    <header className="container">
+      <h1>ACME Product/Categories React</h1>
+    </header>
+    <section>
+      <ProductList />
+    </section>
+  )
+};
+
+export default App;
